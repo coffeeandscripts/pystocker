6f34991cedf64b1ee7b7c212374dec9bd374fb06
Erin Saricilar <ersari94@gmail.com>
2016-07-05 11:39:40 +1000

    Initializing ncurses window

--- pystock.py
+++ pystock.py
@@ -1,10 +1,40 @@
-#!/user/bin/env python
+#!/usr/bin/env python
 
 ## IMPORTS ##
+import curses
+import ystockquote
 
 ## GLOBALS ##
+x = 1
 
 ## FUNCTIONS ##
+#initialize the curses window and return scr
+def init_scr():
+    
+    scr = curses.initscr()
+
+    curses.noecho()
+    curses.curs_set(0)
+    scr.keypad(True)
+    scr.clear()    
+
+    return scr
+
+#user scr to terminate the window and revert back to terminal
+def term_scr(scr):
+
+    curses.nocbreak()
+    scr.keypad(False)
+    curses.echo()
+    curses.endwin()
 
 ## WORKFLOW ##
+scr = init_scr()
+
 #main loop
+while x != ord("0"):
+    scr.refresh()
+    x = scr.getch()
+
+#terminating the window
+term_scr(scr)
