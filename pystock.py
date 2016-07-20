b1bd2323c218fb1642b05b3b630396026563b356
Erin Saricilar <ersari94@gmail.com>
2016-07-20 12:32:51 +1000

    Created windows that feature information

--- pystock.py
+++ pystock.py
@@ -52,6 +52,53 @@ def open_option_window(scr_dim):
 
     return win
 
+#creates the windows at the top, left and main segments
+def open_top(scr_dim):
+    
+    top_scr = curses.newwin(4, scr_dim[1], 0, 0)
+
+    return top_scr
+
+def open_left(scr_dim):
+
+    left_scr = curses.newwin(scr_dim[0]-5, 10, 5, 0)
+
+    return left_scr
+
+def open_main(scr_dim):
+
+    main_scr = curses.newwin(scr_dim[0]-5, scr_dim[1]-10, 5, 10)
+
+    return main_scr
+
+def open_strip(scr_dim):
+
+    strip_scr = curses.newwin(1, scr_dim[1], 4, 0)
+
+    return strip_scr
+
+def window_colors(scr_top, scr_strip, scr_left, scr_main):
+
+    curses.start_color()
+
+    curses.init_pair(1,curses.COLOR_WHITE,curses.COLOR_BLUE)
+    curses.init_pair(2,curses.COLOR_WHITE,curses.COLOR_RED)
+    curses.init_pair(3,curses.COLOR_WHITE,curses.COLOR_GREEN)
+    curses.init_pair(4,curses.COLOR_WHITE,curses.COLOR_MAGENTA)
+
+    scr_top.bkgd(curses.color_pair(1))
+    scr_strip.bkgd(curses.color_pair(4))
+    scr_left.bkgd(curses.color_pair(2))
+    scr_main.bkgd(curses.color_pair(3))
+
+#refreshes the visible windows in order
+def refresh_windows(scr_top, scr_strip, scr_left, scr_main):
+
+    scr_top.refresh()
+    scr_strip.refresh()
+    scr_left.refresh()
+    scr_main.refresh()
+
 
 ## WORKFLOW ##
 scr = init_scr()
@@ -79,7 +126,15 @@ while x != ord("0"):
     if option_window_open == True:
         win.border(0)
 
+    scr_top = open_top(scr_dim)
+    scr_left = open_left(scr_dim)
+    scr_main = open_main(scr_dim)
+    scr_strip = open_strip(scr_dim)
+
+    window_colors(scr_top, scr_strip, scr_left, scr_main)
+
     scr.refresh()
+    refresh_windows(scr_top, scr_strip, scr_left, scr_main)
 
     if option_window_open == True:
         win.refresh()
