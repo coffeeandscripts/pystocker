d9a4e6973c1d87605583432e61694bcea7fed93d
Erin Saricilar <ersari94@gmail.com>
2016-08-03 22:13:04 +1000

    Added historical data for 1 year total

--- pystocker/get_data.py
+++ pystocker/get_data.py
@@ -7,9 +7,11 @@ Loop in the background to pull data.
 ## IMPORTS ##
 import curses
 import os
+import datetime
 
 #user generated imports
 from pystocker import *
+from pystocker import ystockquote
 
 ## CLASSES ##
 
@@ -28,6 +30,11 @@ def write_stock_data(stock_data_dict):
         f.write(str(stock_data_dict))
     f.close()
 
+def write_hist_data(hist_data_dict):
+    with open(root_path + "/.pystocker/hist_data", "w") as f:
+        f.write(str(hist_data_dict))
+    f.close()
+
 
 ## WORKFLOW ##
 
@@ -35,7 +42,10 @@ os.chdir(root_path)
 
 x = 1
 
+historicals = 0
+
 stock_data_dict = {}
+hist_data_dict = {}
 
 row1 = {}
 row2 = {}
@@ -49,7 +59,7 @@ while x == 1:
     stock_data_dict.clear()
 
     for stock in stock_list:
-        data = stocks.fetch_stock_data(str(stock))
+        data = stocks.fetch_stock_data(stock[:-1])
         stock_data_dict = prepare_stock_dict(stock, data, stock_data_dict)
 
     write_stock_data(stock_data_dict)
@@ -65,3 +75,24 @@ while x == 1:
         counter = counter + 1
 
     permanents.write_perm_data(perm_data_dict)
+
+    if historicals == 1:
+        for stock in stock_list:
+            if stock[:-1] not in hist_data_dict:
+                if stock not in hist_data_dict:
+                    historicals = 0
+        if historicals_get_datetime < datetime.datetime.now() - datetime.timedelta(minutes=30):
+            historicals = 0
+
+    if historicals == 0:
+        hist_data_dict.clear()
+
+        for stock in stock_list:
+            hist_data = stocks.fetch_historical_data(stock[:-1])
+            hist_data_dict = prepare_stock_dict(stock[:-1], hist_data, hist_data_dict)
+
+        write_hist_data(hist_data_dict)
+        
+        historicals_get_datetime = datetime.datetime.now()
+
+        historicals = 1
