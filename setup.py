63ff64dfde0ce9fce3e507cb1a87a86becd03133
ersari94 <ersari94@gmail.com>
2017-02-18 14:27:24 +1100

    Reduced reload times

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pystocker',
-        version='0.1.19',
+        version='0.1.20',
         description='lightweight ncurses stock tracker',
         url='http://github.com/coffeeandscripts/pystock',
         author='coffeeandscripts',
