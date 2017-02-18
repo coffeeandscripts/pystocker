452412fbc5efca668516cdca74f591902ef0b523
ersari94 <ersari94@gmail.com>
2017-02-18 12:32:15 +1100

    Fixed price in ystockquote

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pystocker',
-        version='0.1.16',
+        version='0.1.17',
         description='lightweight ncurses stock tracker',
         url='http://github.com/coffeeandscripts/pystock',
         author='coffeeandscripts',
